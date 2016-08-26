376698c03229eb1d57bdeb6556fa8278d4262545
hubert0527 <hubert052702@gmail.com>
2016-08-27 03:42:27 +0800

    create testing

--- ChatRoomPackage/ChatRoom.js
+++ ChatRoomPackage/ChatRoom.js
@@ -1,3 +1,7 @@
+
+var tempServerUrl;
+
+var socketInst;
 /**
  * Created by hubert lin2 on 2016/8/14.
  */
@@ -14,25 +18,82 @@
 function ChatRoom(ctx, settings) {
     this.ip = settings['ip'];
     this.port = settings['port'];
+    this.serverUrl = settings['url'];
     this.style = settings['style'];
 
+    var url = this.serverUrl ? this.serverUrl : this.ip + ':' + this.port;
+
+    createSocket(url);
+
     // insert simple html layout for context
-    prepareContextLayout(ctx,settings);
+    // use callback cuz loading items is asynchronized
+    prepareContextLayout(ctx,settings,function () {
+        // add controls to the items in context, like buttons or something else
 
-    // add controls to the items in context, like buttons or something else
+        createWidgetFunction();
 
-    // BLABLABLA still constructing
+        $('#getBtn').click(function () {
+            // doGet(url);
+        });
 
+    });
+
+}
+
+function createSocket(url) {
+
+    console.log('create socket: ws://'+url);
+
+    socketInst = new WebSocket("ws://"+url);
+
+    socketInst.onerror = function(e) {
+        socketInst = null;
+        console.log('create socket FAIL!');
+        $('#serverResponse').text('ERROR, server unreachable!');
+    };
+
+    socketInst.onmessage = function(e) {
+        $('#serverResponse').text(e.data);
+        console.log('server respond: ' + e.data);
+    };
+
+    socketInst.onopen = function() {
+        console.log("create socket SUCCESS! Send 'Hello, world!' ");
+        socketInst.send("Hello, world!");
+    };
+}
+
+/**
+ * This project will cause 'cross-origin' problem, more info:
+ * http://jimmy319.blogspot.tw/2013/09/javascript-xhr-cross-domain-request-is.html
+ * http://www.wengweitao.com/corskua-yu-zi-yuan-gong-xiang-cross-origin-resource-sharing.html
+ */
+function doGet(url) {
+    $.ajax({
+        url: 'http://'+url,
+        success: function (data) {
+            console.log('receive ' + data.toString());
+        },
+        dataType: 'text'
+    });
+
+    // var req = new XMLHttpRequest();
+    // req.open('GET', 'http://'+url+'/hello/', true);
+    // req.onreadystatechange = function (data) {
+    //     console.log('receive ' + data.toString());
+    // };
+    // req.send();
 }
 
 /**
  * Insert layout for context object
  */
-function prepareContextLayout(ctx,settings) {
+function prepareContextLayout(ctx,settings,callback) {
 
     $.get( "./ChatRoomPackage/ChatRoom.html", function( data ) {
         ctx.innerHTML = data;
         loadScriptsForChatRoomHtml(settings);
+        if(callback) callback();
     });
 
 }
@@ -71,7 +132,51 @@ function createClickOnTopBarPopUpAnimation() {
     });
 }
 
+/**
+ * create functionality for widgets in chat room
+ */
+function createWidgetFunction() {
+    createServerSettingWidget();
+    createSendingMessageWidget();
+}
 
+function createServerSettingWidget() {
+    $('#serverSettingButton').click(function () {
+        tempServerUrl = $('#serverInput').val();
+        reconnect(tempServerUrl);
+    });
+    $('#serverInput').keyup(function (k) {
+        if(k.keyCode==13){
+            tempServerUrl = $('#serverInput').val();
+            reconnect(tempServerUrl);
+        }
+    });
+}
+
+function createSendingMessageWidget() {
+    $('#submitMessageButton').click(function () {
+        sendMessage();
+    });
+    $('#messageInput').keyup(function (k) {
+        if(k.keyCode==13){
+            sendMessage();
+        }
+    });
+}
+
+function sendMessage() {
+    var str = $('#messageInput').val();
+    console.log('send ' + str + ' to server!');
+    if(str && socketInst){
+        $('#serverResponse').text('sending: '+str + ' ...');
+        socketInst.send(str);
+    }
+}
+
+function reconnect(target) {
+    if(socketInst) socketInst.close();
+    createSocket(target);
+}
 
 /**
  * Simple toString method for debugging
--- js/main.js
+++ js/main.js
@@ -4,9 +4,8 @@ $(document).ready(function () {
     var settings = {
         ip: '1.2.3.4',
         port: '1234',
+        url:'intense-waters-75416.herokuapp.com/chat/',
         style: 'default'
     };
     var chatRoom = new ChatRoom(ctx,settings);
-
-    console.log(chatRoom.toString());
 });
