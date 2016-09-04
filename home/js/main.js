2f6a31fbd6bcb945b2d6b4367c6bd2ab8dfdc0a8
hubert0527 <hubert052702@gmail.com>
2016-09-05 04:57:58 +0800

    disable hover on smart phone

--- home/js/main.js
+++ home/js/main.js
@@ -178,18 +178,40 @@ function dealAllAboutMe() {
 }
 
 function browserDetect() {
-        // Opera 8.0+
+    // Opera 8.0+
     window.isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-        // Firefox 1.0+
+    // Firefox 1.0+
     window.isFirefox = typeof InstallTrigger !== 'undefined';
-        // At least Safari 3+: "[object HTMLElementConstructor]"
+    // At least Safari 3+: "[object HTMLElementConstructor]"
     window.isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-        // Internet Explorer 6-11
+    // Internet Explorer 6-11
     window.isIE = /*@cc_on!@*/false || !!document.documentMode;
-        // Edge 20+
+    // Edge 20+
     window.isEdge = !isIE && !!window.StyleMedia;
-        // Chrome 1+
+    // Chrome 1+
     window.isChrome = !!window.chrome && !!window.chrome.webstore;
+
+    // touch device
+    var touch = window.ontouchstart
+            || (navigator.MaxTouchPoints > 0)
+            || (navigator.msMaxTouchPoints > 0);
+    if (touch) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
+
 }
 
 function grabMouseScroll() {
