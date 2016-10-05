d467700bf571a482963851fe8c6e6554c2bb8f1a
hubert0527 <hubert052702@gmail.com>
2016-10-05 15:59:58 +0800

    cross domain must https

--- myProjects/ChatRoom/ChatRoomPackage/ChatRoom.js
+++ myProjects/ChatRoom/ChatRoomPackage/ChatRoom.js
@@ -205,9 +205,9 @@
 
     function createSocket(url) {
 
-        var wsOption = window.location.protocol == 'https:' ? {secure: true} : {};
-        var socket = io.connect('http://'+url,wsOption);
-        console.log('create socket: '+url);
+        var ws_scheme = window.location.protocol;
+        var socket = io.connect(ws_scheme+'//'+url);
+        console.log('create socket: '+ ws_scheme +'//'+url);
 
         socket.on('create', function(data){
             console.log("create socket SUCCESS! Send 'Hello, world!' ");
